bc5123ac41859104e22a0996063b91d116e773ac
Brandon3522 <Brandonsitz17@gmail.com>
2023-01-26 21:28:37 -0600

    Removed unused libraries

--- CSCI4600Project/MainWindow.xaml.cs
+++ CSCI4600Project/MainWindow.xaml.cs
@@ -1,23 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Windows.Threading;
 using System.Xml;
-using System.Xml.Linq;
 using System.Xml.Serialization;
-using System.Xml.XPath;
 
 namespace CSCI4600Project
 {
